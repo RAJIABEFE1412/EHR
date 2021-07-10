bbae11908f6232d9fe9a499be5ddeecb1098787e
Raji Rasheed <base36.rr@gmail.com>
2021-07-10 15:58:50 +0100

    Nope

--- index.js
+++ index.js
@@ -164,6 +164,7 @@ io.on('connection', socket => {
     socket.on('getHistories', msg => {
         // console.log("Are you getting here.....");
         var data =  JSON.stringify(msg)
+        console.log("data.... "data)
 
         var res = bc.getBlock(data.hash, 1);
 
